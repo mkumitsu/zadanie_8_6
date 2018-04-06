9c3792aaba4bce8485f78d89fcff8c614cd1d859
Maciek Bulski <fragmast3r@gmail.com>
2018-04-06 19:40:30 +0100

    fixed vars & formatting

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 var a = 2;
-b = 5;
-value = (a * a) - (2 * a * b) - (b * b);
+var b = 5;
+var value = (a * a) - (2 * a * b) - (b * b);
 console.log(value);
 prompt('Wynik działania to: '+ value);
 if (value < 0) {
-console.log('wynik ujemny');
- prompt('wynik ujemny');  
+	console.log('wynik ujemny');
+	prompt('wynik ujemny');  
 }
 else if (value > 0) {
-  console.log('wynik dodatni');
-  prompt('wynik dodatni'); 
+	console.log('wynik dodatni');
+	prompt('wynik dodatni'); 
 }
 else {
-  console.log('wynik równa się 0');
-  prompt('wynik równa się 0'); 
+	console.log('wynik równa się 0');
+	prompt('wynik równa się 0'); 
 }
